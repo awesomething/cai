612ecaa8a846d5e66180ffdd983a431fa10e6d71
awesomething <79168736+PairApp@users.noreply.github.com>
2023-05-10 21:29:29 -0400

    add adaptable plus

--- src/state/api.js
+++ src/state/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const url = "https://meetsupport.adaptable.app/"
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL, url }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL + url }),
     reducerPath: "main",
     tagTypes: [],
     endpoints: (build) => ({
